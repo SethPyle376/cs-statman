c20559c3036510affbd40870a46dec63713adfca
Seth Pyle <seth.pyle86@gmail.com>
2020-10-20 07:55:53 -0600

    file upload testing stuff

--- uploadserver/uploadserver.go
+++ uploadserver/uploadserver.go
@@ -2,6 +2,7 @@ package uploadserver
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -13,12 +14,45 @@ func New() (*UploadServer, error) {
 }
 
 func (us *UploadServer) handleUpload(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Uploading file")
+	fmt.Fprintf(w, "Uploading file\n")
+
+	// 50Mb max
+	r.ParseMultipartForm(50 << 21)
+	file, handler, err := r.FormFile("myFile")
+
+	if err != nil {
+		fmt.Fprintf(w, "Error getting file\n")
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	defer file.Close()
+
+	fmt.Fprintf(w, "File size: %d\n", handler.Size)
+
+	tempfile, err := ioutil.TempFile("temp", "upload-*.png")
+
+	if err != nil {
+		fmt.Fprintf(w, "error getting temp file\n")
+		fmt.Fprintf(w, err.Error()+"\n")
+		return
+	}
+	defer tempfile.Close()
+
+	filebytes, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		fmt.Fprintf(w, "error reading file to bytes\n")
+		return
+	}
+
+	tempfile.Write(filebytes)
+	fmt.Fprintf(w, "successfully uploaded file\n")
 }
 
 func (us *UploadServer) Start() {
 	http.HandleFunc("/upload", us.handleUpload)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8081", nil)
 }
 
 func (us *UploadServer) Stop() { /* TODO */ }
