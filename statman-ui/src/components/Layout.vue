0beb4dc7fa77c07c209dbfa2a466a0c890e2a087
Seth Pyle <seth.pyle86@gmail.com>
2020-11-06 14:29:22 -0700

    link to upload page

--- statman-ui/src/components/Layout.vue
+++ statman-ui/src/components/Layout.vue
@@ -5,6 +5,18 @@
       <v-toolbar-title class="primary--text">Statman</v-toolbar-title>
     </v-app-bar>
     <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense nav color="3c3836">
+        <v-list-item to="/upload">
+          <v-list-item-icon>
+            <v-icon color="primary">mdi-cloud-upload</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="primary--text">
+              Upload Demos
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
     <v-main>
       <router-view/>
